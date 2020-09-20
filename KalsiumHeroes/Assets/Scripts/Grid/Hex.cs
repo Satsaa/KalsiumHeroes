c66b8373bf27c705eda5cea8d269258f401d6f2d
Samuel <32210043+Satsaa@users.noreply.github.com>
2020-09-20 05:04:26 +0300

    Basic functions for grids

--- KalsiumHeroes/Assets/Scripts/Grid/Hex.cs
+++ KalsiumHeroes/Assets/Scripts/Grid/Hex.cs
@@ -5,29 +5,7 @@ using Muc.Extensions;
 using UnityEngine;
 
 [Serializable]
-public struct Hex {
-
-  [field: SerializeField] public int x { get; }
-  [field: SerializeField] public int y { get; }
-  [field: SerializeField] public int z { get; }
-
-  public Hex up => new Hex(x, y, z);
-  public Hex down => new Hex(x, y, z);
-
-  public Hex upRight => new Hex(x, y, z);
-  public Hex downRight => new Hex(x, y, z);
-  public Hex upLeft => new Hex(x, y, z);
-  public Hex downLeft => new Hex(x, y, z);
-
-  /// <summary> Neighbors from downRight to upRight </summary>
-  public IEnumerable<Hex> Neighbors() {
-    yield return downRight;
-    yield return down;
-    yield return downLeft;
-    yield return upLeft;
-    yield return up;
-    yield return upRight;
-  }
+public partial struct Hex {
 
   public Hex(int x, int y, int z) {
     this.x = x;
@@ -35,5 +13,17 @@ public struct Hex {
     this.z = z;
     if (x + y + z != 0) throw new ArgumentException($"The added total of {nameof(x)}, {nameof(y)} and {nameof(z)} must be zero");
   }
+
+  [field: SerializeField] public int x { get; }
+  [field: SerializeField] public int y { get; }
+  [field: SerializeField] public int z { get; }
+
+  public Hex up => new Hex(x, y + 1, z - 1);
+  public Hex down => new Hex(x, y - 1, z + 1);
+
+  public Hex upRight => new Hex(x + 1, y, z - 1);
+  public Hex downRight => new Hex(x + 1, y - 1, z);
+  public Hex upLeft => new Hex(x - 1, y + 1, z);
+  public Hex downLeft => new Hex(x - 1, y, z + 1);
 
 }
